f2b1822d552fc241795765d2e2499a39c1bdf21e
song <syt-aini@126.com>
2021-03-15 13:52:43 +0800

    add common func

--- common/common.cpp
+++ common/common.cpp
@@ -1,160 +1,230 @@
-#include "stdafx.h"
+#include "StdAfx.h"
 #include "common.h"
 
+#include <Windows.h>
 #include <memory.h>
 #include <ctype.h>
+#include <assert.h>
+
+
+
+// unicode转ansi
+string UnicodeToAnsi(wstring in)
+{
+    string s(in.length()*2+1,' ' );
+    size_t len = ::WideCharToMultiByte(CP_ACP, 0,
+        in.c_str(), in.length(),
+        &s[0], s.length(),
+        NULL, NULL);
+    s.resize(len);
+    return s;
+}
+
+// ansi转unicode
+wstring AnsiToUnicode(string in)
+{
+    wstring s(in.length(), _T(' ' ));
+    size_t len = ::MultiByteToWideChar(CP_ACP, 0,
+        in.c_str(), in.length(),
+        &s[0], s.length());
+    s.resize(len);
+    return s;
+}
+
+// 宽 字符转UTF8
+string EncodeUtf8(wstring in)
+{
+    string s(in.length()*3+1,' ' );
+    size_t len = ::WideCharToMultiByte(CP_UTF8, 0,
+        in.c_str(), in.length(),
+        &s[0], s.length(),
+        NULL, NULL);
+    s.resize(len);
+    return s;
+}
+
+// UTF8 转宽字符
+wstring DecodeUtf8(string in)
+{
+    wstring s(in.length(), _T(' ' ));
+    size_t len = ::MultiByteToWideChar(CP_UTF8, 0,
+        in.c_str(), in.length(),
+        &s[0], s.length());
+    s.resize(len);
+    return s;
+}
+
+unsigned char ToHex(unsigned char x)
+{
+	return x > 9 ? x + 55 : x + 48;
+}
+
+unsigned char FromHex(unsigned char x)
+{
+	unsigned char y;
+	if ( x >= 'A' && x <= 'Z')
+	{
+		y = x - 'A' + 10;
+	}
+	else if ( x >= 'a' && x <= 'z')
+	{
+		y = x - 'a' + 10;
+	}
+	else if (x >= '0' && x <= '9')
+	{
+		y = x - '0';  
+	}
+	else
+	{
+		assert(0);
+	}
+	return y;
+}
+
+string UrlEncode(const string& str)
+{
+	string strTemp = "";
+	size_t length = str.length();
+	for ( size_t i = 0;i < length;i++ )
+	{
+		if ( isalnum((unsigned char)str[i]) || (str[i] == '-') || (str[i] == '_') || (str[i] == '.') || (str[i] == '~') )
+		{
+			strTemp += str[i];
+		}
+		else if ( str[i] == ' ')
+		{
+			strTemp += "+";
+		}
+		else
+		{
+			strTemp += '%';
+			strTemp += ToHex((unsigned char)str[i] >> 4);
+			strTemp += ToHex((unsigned char)str[i] % 16);
+		}
+	}
+	return strTemp;
+}
+
+string UrlDecode(const string& str)
+{
+	string strTemp = "";
+	size_t length = str.length();
+	for ( size_t i = 0;i < length;i++ )
+	{
+		if ( str[i] == '+' )
+		{
+			strTemp += ' ';
+		}
+		else if ( str[i] == '%' )
+		{
+			assert(i + 2 < length);
+			unsigned char high = FromHex((unsigned char)str[++i]);
+			unsigned char low  = FromHex((unsigned char)str[++i]);
+			strTemp += high*16 + low;
+		}
+		else 
+		{
+			strTemp += str[i];
+		}
+	}
+	return strTemp;
+}
+
+
 
 unsigned char* CharToBin(const unsigned char *pszSrc, int nSrcLen, unsigned char *pszDest, int *nDestLen)
 {
-    memset(pszDest, 0, *nDestLen);
+	memset(pszDest, 0, *nDestLen);
 
-    if (nSrcLen/2 > *nDestLen)
-        return pszDest;
+	if (nSrcLen/2 > *nDestLen)
+		return pszDest;
 
-    unsigned char ch;
-    int i=0;
-    int j=0;
+	unsigned char ch;
+	int i=0;
+	int j=0;
 
-    for (i=0; i<nSrcLen; i++)
-    {
-        ch = pszSrc[i];
+	for (i=0; i<nSrcLen; i++)
+	{
+		ch = pszSrc[i];
 
-        if (isdigit(ch)>0)
-        {
-            //数字
-            if (i%2 > 0) //i为奇数位  放至低位
-            {
-                pszDest[j] |= (ch & 0x0F);
-                j++;
-            }
-            else //i为偶数位 放至高位
-            {
-                pszDest[j] |= (ch & 0x0F)<<4;
-            }
-        }
-        else
-        {
-            //字母
-            ch = toupper(ch);
-            if (i%2 > 0)
-            {
-                pszDest[j] |= ((ch & 0x0F) + 9);
-                j++;
-            }
-            else
-            {
-                pszDest[j] |= ((ch & 0x0F) + 9)<<4;
-            }
-        }
-    }
+		if (isdigit(ch)>0)
+		{
+			//数字
+			if (i%2 > 0) //i为奇数位  放至低位
+			{
+				pszDest[j] |= (ch & 0x0F);
+				j++;
+			}
+			else //i为偶数位 放至高位
+			{
+				pszDest[j] |= (ch & 0x0F)<<4;
+			}
+		}
+		else
+		{
+			//字母
+			ch = toupper(ch);
+			if (i%2 > 0)
+			{
+				pszDest[j] |= ((ch & 0x0F) + 9);
+				j++;
+			}
+			else
+			{
+				pszDest[j] |= ((ch & 0x0F) + 9)<<4;
+			}
+		}
+	}
 
-    *nDestLen = j;
+	*nDestLen = j;
 
-    return pszDest;
+	return pszDest;
 }
 
 
 unsigned char* BinToChar(const unsigned char *pszSrc, int nSrcLen, unsigned char *pszDest, int *nDestLen)
 {
-    memset(pszDest, 0, *nDestLen);
+	memset(pszDest, 0, *nDestLen);
 
-    if (nSrcLen*2 > *nDestLen)
-        return pszDest;
+	if (nSrcLen*2 > *nDestLen)
+		return pszDest;
 
-    unsigned char ch;
-    int i=0;
-    int j=0;
+	unsigned char ch;
+	int i=0;
+	int j=0;
 
-    for (i=0; i<nSrcLen; j++)
-    {
-        ch = pszSrc[i];
-        if (j%2 == 0)
-        {
-            ch = (pszSrc[i]>>4) & 0x0F; //高位
-            if ((ch>=0) && (ch <=9))
-            {
-                pszDest[j] = ch | 0x30 ;
-            }
-            else
-            {
-                pszDest[j] = (ch - 9) | 0x40;
-            }
-        }
-        else
-        {
-            ch = pszSrc[i] & 0x0F; //低位
-            if ((ch>=0) && (ch <=9))
-            {
-                pszDest[j] = ch | 0x30 ;
-            }
-            else
-            {
-                pszDest[j] = (ch - 9) | 0x40;
-            }
-
-            i++;
-        }
-    }
-
-    *nDestLen = j;
-
-    return pszDest;
-}
-
-string UnicodeToAnsi(wstring in)
-{
-	string s(in.length()*2+1,' ' );
-	size_t len = ::WideCharToMultiByte(CP_ACP, 0,
-		in.c_str(), in.length(),
-		&s[0], s.length(),
-		NULL, NULL);
-	s.resize(len);
-	return s;
-}
-
-void WriteLogMsg(char chLogMsg[])
-{
-
-	char strFilePath[40] = ".\\Log\\";//如果是"\\Log\\"则到了当前盘符的根目录下了。
-	char strTimeFileName[20];//将当前时间转换成字符串---声明字符串长度的时候，要比实际长度多1，作为结尾符号
-
-
-	SYSTEMTIME sysTime; 
-	GetLocalTime( &sysTime ); //得到系统时间
-	sprintf(strTimeFileName,"%d-%d-%d",sysTime.wYear,sysTime.wMonth,sysTime.wDay);//"2010-09-21"
-
-	strcat(strTimeFileName,".logFile");//加上扩展名--登录日志
-	strcat(strFilePath,strTimeFileName);//得到完整的路径名
-
-
-	FILE *fp;//文件指针
-
-	if ((fp=fopen(strFilePath,"a"))==NULL)//以追加的形式往文件中写东西
+	for (i=0; i<nSrcLen; j++)
 	{
-		//如果打开不成功，则一般表示没有Log目录
-		//创建Log目录，然后再重新打开--一般情况下，如果目录存在的话，就不会创建成功的。
-		if(!CreateDirectory(_T(".\\Log\\"),NULL)) 
-		{ 
-			return;
-		}
-		else 
+		ch = pszSrc[i];
+		if (j%2 == 0)
 		{
-			if ((fp=fopen(strFilePath,"a"))==NULL)//以追加的形式往文本文件中写东西
-			{			
-				exit(0);
+			ch = (pszSrc[i]>>4) & 0x0F; //高位
+			if ((ch>=0) && (ch <=9))
+			{
+				pszDest[j] = ch | 0x30 ;
 			}
-		} 
+			else
+			{
+				pszDest[j] = (ch - 9) | 0x40;
+			}
+		}
+		else
+		{
+			ch = pszSrc[i] & 0x0F; //低位
+			if ((ch>=0) && (ch <=9))
+			{
+				pszDest[j] = ch | 0x30 ;
+			}
+			else
+			{
+				pszDest[j] = (ch - 9) | 0x40;
+			}
+
+			i++;
+		}
 	}
 
-	char strTimeTag[30]; //将时间转成字符串
-	sprintf(strTimeTag,"%d-%d-%d  %d:%d:%d  ",sysTime.wYear,sysTime.wMonth,sysTime.wDay,
-		sysTime.wHour,sysTime.wMinute,sysTime.wSecond);
+	*nDestLen = j;
 
-	fputs(strTimeTag,fp);//写入时间标记
-	fputs(" : ",fp);//分隔符号
-	fputs(chLogMsg,fp);//写入消息日志
-	fputs("\n",fp);//换行
-
-	fclose(fp);
-
+	return pszDest;
 }
--- common/common.h
+++ common/common.h
@@ -2,12 +2,45 @@
 #define _COMMON_H_20130423104102
 
 #include <string>
-using namespace std;
-    
+
+using std::string;
+using std::wstring;
+
+#if !defined(SAFE_RELEASE)
+#define SAFE_RELEASE(p)		{ if((p) != NULL) { (p)->Release(); (p)=NULL; } }
+#endif
+
+#if !defined(SAFE_DELETE)
+#define SAFE_DELETE(p)		{ if((p) != NULL) { delete (p);     (p)=NULL; } }
+#endif
+
+#if !defined(SAFE_DELETEARRAY)
+#define SAFE_DELETEARRAY(p)	{ if((p) != NULL) { delete [](p);	(p) = NULL;} }
+#endif
+
+// unicode转ansi
+string UnicodeToAnsi(wstring in);
+
+// ansi转unicode
+wstring AnsiToUnicode(string in);
+
+// 宽 字符转UTF8
+string EncodeUtf8(wstring in);
+
+// UTF8 转宽字符
+wstring DecodeUtf8(string in);
+
+unsigned char ToHex(unsigned char x);
+
+unsigned char FromHex(unsigned char x);
+
+string UrlEncode(const string& str);
+
+string UrlDecode(const string& str);
+
+
 unsigned char* CharToBin(const unsigned char *pszSrc, int nSrcLen, unsigned char *pszDest, int *nDestLen);
 unsigned char* BinToChar(const unsigned char *pszSrc, int nSrcLen, unsigned char *pszDest, int *nDestLen);
-string UnicodeToAnsi(wstring in);
-void WriteLogMsg(char chLogMsg[]);
 
 
 #endif
